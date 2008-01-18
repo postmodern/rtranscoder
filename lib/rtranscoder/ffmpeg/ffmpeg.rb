fb552f395e2f60ec0449e043bba3f52b00fcb491
Postmodern <postmodern.mod3@gmail.com>
2008-01-18 05:08:48 +0000

    * Documentation.
    git-svn-id: svn://rubyforge.org/var/svn/rtranscoder/trunk@5 2162644f-53e9-4102-9b32-7e08c367be7b

--- lib/rtranscoder/ffmpeg/ffmpeg.rb
+++ lib/rtranscoder/ffmpeg/ffmpeg.rb
@@ -8,22 +8,46 @@ module RTranscoder
 
     name_program 'ffmpeg'
 
+    #
+    # Creates a new FFmpeg object with the specified _path_ of the +ffmpeg+
+    # program.
+    #
     def initialize(path)
       super(path)
     end
 
+    #
+    # See FFmpeg#encode.
+    #
     def self.encode(options={},&block)
       self.create.encode(options,&block)
     end
 
+    #
+    # See FFmpeg#thumbnail.
+    #
     def self.thumbnail(options={})
       self.create.thumbnail(options)
     end
 
+    #
+    # Runs +ffmpeg+ with the given _options_ and _block_ using FFmpegTask.
+    #
     def encode(options={},&block)
       run_with_task(FFmpegTask.new(options,&block))
     end
 
+    #
+    # Extracts a thumbnail using the given _options_.
+    #
+    # _options_ may contain the following keys:
+    # <tt>:video</tt>:: The input video file.
+    # <tt>:start</tt>:: The start timestamp of the thumbnail.
+    # <tt>:length</tt>:: The duration time of the thumbnail.
+    # <tt>:fps</tt>:: The FPS to use, defaults to 1.
+    # <tt>:dimension</tt>:: The dimension of the thumbnail.
+    # <tt>:image</tt>:: The output thumbnail image.
+    #
     def thumbnail(options={})
       encode do |ffmpeg|
         ffmpeg.input = options[:video]
--- lib/rtranscoder/ffmpeg/rtranscoder.rb
+++ lib/rtranscoder/ffmpeg/rtranscoder.rb
@@ -1,6 +1,9 @@
 require 'rtranscoder/ffmpeg/ffmpeg'
 
 module RTranscoder
+  #
+  # See FFmpeg.thumbnail.
+  #
   def RTranscoder.thumbnail(options={})
     FFmpeg.thumbnail(options)
   end
