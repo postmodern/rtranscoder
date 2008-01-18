66d9812e9571e3277ecba39252a068aac05516c4
Postmodern <postmodern.mod3@gmail.com>
2008-01-18 04:53:48 +0000

    * Added documentation.
    git-svn-id: svn://rubyforge.org/var/svn/rtranscoder/trunk@2 2162644f-53e9-4102-9b32-7e08c367be7b

--- lib/rtranscoder/extensions/float.rb
+++ lib/rtranscoder/extensions/float.rb
@@ -1,13 +1,28 @@
 class Float
 
+  #
+  # Returns the +Float+ in Hz as an +Integer+.
+  #
+  #   25.05.Hz # => 2505
+  #
   def Hz
     self.to_i
   end
 
+  #
+  # Returns the +Float+ in kHz as an +Integer+.
+  #
+  #   1.337.kHz # => 1337
+  #
   def kHz
     (self * 1000).Hz
   end
 
+  #
+  # Returns the +Float+ in kHz as an +Integer+.
+  #
+  #   2.6.mHz # => 2600000
+  #
   def mHz
     (self * 1000_000).Hz
   end
