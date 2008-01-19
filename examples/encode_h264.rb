83178284903387f6d38e531c76beab540a7d96ee
Postmodern <postmodern.mod3@gmail.com>
2008-01-19 06:03:35 +0000

    * Added mencoder h264 example.
    git-svn-id: svn://rubyforge.org/var/svn/rtranscoder/trunk@18 2162644f-53e9-4102-9b32-7e08c367be7b

---
+++ examples/encode_h264.rb
@@ -0,0 +1,18 @@
+require 'rtranscoder/mplayer'
+
+include RTranscoder
+
+#
+# Encode an input AVI file using the x264 codec.
+# Equivalent to:
+#   mencoder input.avi -o output.avi -ovc x264 -x264encopts bitrate=3000
+#     pass=1 nr=2000
+#
+MEncoder.encode do |mencoder|
+  mencoder.input = 'input.avi'
+  mencoder.output_video_codec = :x264
+  mencoder.x264enc.bitrate = 3000
+  mencoder.x264enc.pass = 1
+  mencoder.x264enc.nr = 2000
+  mencoder.output = 'output.avi'
+end
