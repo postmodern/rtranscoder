19d0139ea70418b6b59c40c2d63b1ad5fb05d034
Postmodern <postmodern.mod3@gmail.com>
2008-01-27 01:37:39 +0000

    * Update examples.
    git-svn-id: svn://rubyforge.org/var/svn/rtranscoder/trunk@30 2162644f-53e9-4102-9b32-7e08c367be7b

--- examples/encode_flv.rb
+++ examples/encode_flv.rb
@@ -11,7 +11,7 @@ FFmpeg.encode do |ffmpeg|
   ffmpeg.input = 'video.avi'
   ffmpeg.audio_sampling_rate = 22.050.kHz
   ffmpeg.audio_bitrate = 32 # kbit/s
-  ffmpeg.video_frame_dimension = 320.by(240)
+  ffmpeg.video_frame_size = '320x240'
   ffmpeg.output_format = :flv
   ffmpeg.output = 'video.flv'
 end
--- examples/thumbnail.rb
+++ examples/thumbnail.rb
@@ -5,7 +5,7 @@ include RTranscoder
 #
 # Extracts a frame at 00:00:03.
 # Equivlanent to:
-#   $ ffmpeg -i video.flv -an -ss 00:00:03 -t 00:00:01 -r 1 -y -s 320x240 \
+#   $ ffmpeg -i video.flv -an -ss 00:00:03 -t 00:00:01 -r 1 -y -s 320x240
 #     video_thumbnail.jpg
 #
 FFmpeg.encode do |ffmpeg|
@@ -13,9 +13,9 @@ FFmpeg.encode do |ffmpeg|
   ffmpeg.disable_audio = true
   ffmpeg.record_start_time = '00:00:03'
   ffmpeg.record_for = '00:00:01'
-  ffmpeg.record_fps = 1
-  ffmpeg.overwrite_file = true
-  ffmpeg.output_dimension = 320.by(240)
+  ffmpeg.fps = 1
+  ffmpeg.overwrite_output_file = true
+  ffmpeg.video_frame_size = '320x240'
   ffmpeg.output = 'video_thumbnail%d.jpg'
 end
 
@@ -25,5 +25,6 @@ end
 RTranscoder.thumbnail(:video => 'video.flv',
                       :start => '00:00:03',
                       :length => '00:00:01',
-                      :dimension => 320.by(240),
+                      :width => 320,
+                      :height => 240,
                       :image => 'thumb%d.jpg')
