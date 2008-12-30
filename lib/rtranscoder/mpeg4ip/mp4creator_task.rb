6aab35a73e98cec9285b3b811f60bac4c005d908
Postmodern <postmodern.mod3@gmail.com>
2008-12-30 01:52:07 +0000

    Added some options documentation.
    git-svn-id: svn://rubyforge.org/var/svn/rtranscoder/trunk@73 2162644f-53e9-4102-9b32-7e08c367be7b

--- lib/rtranscoder/mpeg4ip/mp4creator_task.rb
+++ lib/rtranscoder/mpeg4ip/mp4creator_task.rb
@@ -2,9 +2,28 @@ require 'rprogram/task'
 
 module RTranscoder
   module MPEG4IP
+    #
+    # == MP4Creator Options:
+    #
+    # <tt>-aac-old-file-format</tt>:: <tt>mp4creator.aac_old_file_format</tt>.
+    # <tt>-aac-profile</tt>:: <tt>mp4creator.aac_profile</tt>
+    # <tt>-create</tt>:: <tt>mp4creator.create</tt>
+    # <tt>-extract</tt>:: <tt>mp4creator.extract</tt>
+    # <tt>-delete</tt>:: <tt>mp4creator.delete</tt>
+    # <tt>-hint</tt>:: <tt>mp4creator.hint</tt>
+    # <tt>-interleave</tt>:: <tt>mp4creator.interleave</tt>
+    # <tt>-list</tt>:: <tt>mp4creator.list</tt>
+    # <tt>-mut</tt>:: <tt>mp4creator.mut</tt>
+    # <tt>-mpeg4-video-profile</tt>:: <tt>mp4creator.mpeg4_video_profile</tt>
+    # <tt>-optimize</tt>:: <tt>mp4creator.optimize</tt>
+    # <tt>-rate</tt>:: <tt>mp4creator.rate</tt>
+    # <tt>-use64bitstime</tt>:: <tt>mp4creator.use_64bit_timestamps</tt>
+    # <tt>-verbosity</tt>:: <tt>mp4creator.verbosity</tt>
+    # <tt>-help</tt>:: <tt>mp4creator.help</tt>
+    # <tt>FILE</tt>:: <tt>mp4creator.file</tt>
+    #
     class MP4CreatorTask < RProgram::Task
 
-      # Options
       long_option :flag => '-aac-old-file-format'
       long_option :flag => '-aac-profile', :equals => true
       long_option :flag => '-create', :equals => true
